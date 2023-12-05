5cb702299978320447fdf8b89908fdc05e6b9cac
shinmai1054 <shinmai1054@icloud.com>
2023-12-06 03:40:58 +0900

    画像出力用の関数を追加

--- src/img_proc.rs
+++ src/img_proc.rs
@@ -1,5 +1,5 @@
 use image::imageops::FilterType;
-use image::DynamicImage;
+use image::{DynamicImage, RgbImage};
 
 pub fn rotate_img(img: DynamicImage, angle: u32) -> DynamicImage {
     match angle {
@@ -24,6 +24,20 @@ pub fn letterbox(img: DynamicImage, size: u32, rotate_angle: u32) -> Vec<i16> {
         new_img[base_addr + 1] = i16::from(pixel[1]);
         new_img[base_addr + 2] = i16::from(pixel[2]);
         // new_img[base_addr + 3] = 0x7FFF; // for Debug
+    }
+    new_img
+}
+
+pub fn letterbox_img(img: DynamicImage, size: u32, rotate_angle: u32) -> RgbImage {
+    let resized = img.resize(size, size, FilterType::Nearest);
+    let rotated = rotate_img(resized, rotate_angle);
+
+    let pad_w = rotated.width().abs_diff(size) / 2;
+    let pad_h = rotated.height().abs_diff(size) / 2;
+
+    let mut new_img = RgbImage::new(size, size);
+    for (x, y, &pixel) in rotated.to_rgb8().enumerate_pixels() {
+        new_img.put_pixel(x + pad_w, y + pad_h, pixel);
     }
     new_img
 }
